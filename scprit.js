d7bbe690988826ffa0ea8889086e291dd49d71db
MD Kudrot <kk.mdkudrot@gmail.com>
2025-03-11 23:20:18 +0600

    11/03

---
+++ scprit.js
@@ -0,0 +1,7 @@
+var a = document.querySelector(".clock")
+var b = document.querySelector(".btn")
+
+btn.addEventListener("click",function(){
+    clock.style.backgroundColor = "yellow"
+    console.log("Cliocked")
+})
